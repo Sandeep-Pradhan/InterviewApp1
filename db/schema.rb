7915f2eaa5d27b134baf846bd3c4ae584f50f81f
Sandeep <sandeeppradhan313@gmail.com>
2020-05-20 11:49:07 +0530

    Changed Database to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_19_142452) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "participants", force: :cascade do |t|
     t.string "name"
     t.string "email"
