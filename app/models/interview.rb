3c072a80bf371e9e4f7362b5ea2b71cfeca78ba9
Sandeep <sandeeppradhan313@gmail.com>
2020-05-21 17:37:29 +0530

    Overlapping Interviews Validation Added

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,6 +2,7 @@ class Interview < ApplicationRecord
   validates :round, :participant1, :participant2, :starts_at, :ends_at, presence: true
   validate :participants_should_be_different
   validate :date_cannot_be_in_the_past
+  validate :no_overlap
 
   private
     def participants_should_be_different
@@ -16,4 +17,13 @@ class Interview < ApplicationRecord
         errors.add(:ends_at, "must be after the start of interview") 
       end 
     end
+
+  def no_overlap
+    if (Interview.where("((:s1 BETWEEN starts_at AND ends_at) OR (:e1 BETWEEN starts_at AND ends_at) OR (:s1< starts_at AND :e1>ends_at)) AND (participant1 = :p1 OR participant2 = :p1)", s1:self.starts_at, e1:self.ends_at,p1: self.participant1).where.not(updated_at: self.updated_at).any?)
+      errors.add(:participant1, 'It overlaps another interview')
+    end
+    if (Interview.where("((:s2 BETWEEN starts_at AND ends_at) OR (:e2 BETWEEN starts_at AND ends_at) OR (:s2< starts_at AND :e2>ends_at)) AND (participant1 = :p2 OR participant2 = :p2)", s2:self.starts_at, e2:self.ends_at,p2: self.participant2).where.not(updated_at: self.updated_at).any?)
+      errors.add(:participant2, 'It overlaps another interview')
+    end
+  end
 end
